2abf4277006e1bdbe63ef60148abfc6326799870
shaistamomin2004-arch <shaistamomin2004@gmail.com>
2026-04-30 16:51:49 +0530

    Add files via upload

---
+++ while-loop.py
@@ -0,0 +1,39 @@
+i = 1
+while i < 10:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 10")
+
+print("======odd number======")
+
+x = 1
+while x <= 10:
+    print(x)
+    x+=2
+
+print("======even number======")
+y = 0
+while y <= 10:
+      print(y)
+      y+=2
+
+print("======continue statement======")
+
+j = 0
+while j <= 10 :
+    j+=1
+    if j == 5:
+        continue
+    print(j)
+
+
+print("======break statement======")
+j = 0
+while j <= 10 :
+    j+=1
+    if j == 5:
+        break
+    print(j)
+
+
